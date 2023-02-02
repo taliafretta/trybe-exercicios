12d48508e28f4844d593a72ebb1e6e359d0b86f9
Talia Fretta <frettatalia@gmail.com>
2023-02-02 11:38:57 -0300

    2

--- fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop-for/script.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop-for/script.js
@@ -1,5 +1,13 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-for (let index = 0; index < numbers.length; index+=1) {
-    console.log(numbers[index]);
-}
+//for (let index = 0; index < numbers.length; index+=1) {
+//    console.log(numbers[index]);
+//}
+
+
+let result = 0; 
+
+for (let index=0; index < numbers.length; index +=1) {
+result += numbers[index];
+}  
+console.log(result);
