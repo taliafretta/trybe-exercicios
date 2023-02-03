a182e594efea7bcd7cae2be3c24dc060e5433ee7
Talia Fretta <frettatalia@gmail.com>
2023-02-03 09:56:33 -0300

    fazendo média aritmética com for

--- fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop-for/script.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop-for/script.js
@@ -5,9 +5,20 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 //}
 
 
-let result = 0; 
+// let result = 0; 
 
-for (let index=0; index < numbers.length; index +=1) {
-result += numbers[index];
-}  
-console.log(result);
+//for (let index=0; index < numbers.length; index +=1) {
+//result += numbers[index];
+//}  
+//console.log(result);
+
+
+let sum = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+    sum += numbers[index];
+}
+
+let average = sum / numbers.length; 
+
+console.log(average);
