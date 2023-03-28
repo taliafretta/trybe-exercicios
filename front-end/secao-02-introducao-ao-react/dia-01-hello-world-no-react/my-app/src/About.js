6423f032beb5e07a2fe699ca2788ab2020804116
Talia Fretta <frettatalia@gmail.com>
2023-03-28 19:11:40 -0300

    primeiro componente React de classe

---
+++ front-end/secao-02-introducao-ao-react/dia-01-hello-world-no-react/my-app/src/About.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+class About extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          Talia Fretta
+        </h1>
+        <p>
+          Aprendendo a usar o React!
+        </p>
+        <h2>Minhas Habilidades</h2>
+        <ul>
+          <li>CSS</li>
+          <li>HTML</li>
+          <li>JavaScript</li>
+        </ul>
+      </div>
+    );
+
+  }
+}
+
+export default About;
---
+++ front-end/secao-02-introducao-ao-react/dia-01-hello-world-no-react/my-app/src/App.js
@@ -0,0 +1,12 @@
+import About from './About';
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <About/>
+    )
+  }
+}
+
+export default App;
