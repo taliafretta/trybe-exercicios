d0c281ae175a8f10526b6769b7ba33f0f3a88450
Talia Fretta <frettatalia@gmail.com>
2023-05-18 16:33:46 -0300

    exercicios de redux secao 7 dia 1

---
+++ front-end/secao-07-estado-com-redux/index.js
@@ -0,0 +1,48 @@
+import { legacy_createStore as createStore } from 'redux';
+
+function criarCor() {
+  const oneChar = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
+  let cor = '#';
+  const aleatorio = () => Math.floor(Math.random() * oneChar.length);
+  for (let i = 0; i < 6; i += 1) {
+      cor += oneChar[aleatorio()];
+  }
+  return cor;
+}
+
+const reducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case 'NEXT_COLOR':
+      return {
+        ...state,
+        index: state.index === state.colors.length - 1 ? 0 : state.index - 1,
+      };
+    case 'PREVIOUS_COLOR':
+      return {
+        ...state,
+        index: state.index === 0 ? state.colors.length - 1 : state.index - 1,
+      };
+    default:
+      return state;
+  }
+}
+const store = Redux.createStore(reducer);
+
+const btnPrevious = document.getElementById('previous');
+const btnNext = document.getElementById('next');
+
+btnPrevious.addEventListener('click', () => {
+  store.dispatch({ type: 'PREVIOUS_COLOR' });
+})
+
+btnNext.addEventListener('click', () => {
+  store.dispatch({ type: 'NEXT_COLOR'});
+})
+
+store.subscribe(() => {
+  const { colors, index } = store.getState();
+  document.getElementById('value').innerHTML = colors[index];
+  document.getElementById('container').style.backgroundColor = colors[index];
+})
+
+
