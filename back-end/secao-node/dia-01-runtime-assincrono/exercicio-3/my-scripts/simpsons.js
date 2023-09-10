864808b80dc97bdc349a1a1a96af86aaf41e3d10
Talia Fretta <frettatalia@gmail.com>
2023-09-10 12:01:55 -0300

    buscando simpsons por id

--- back-end/secao-node/dia-01-runtime-assincrono/exercicio-3/my-scripts/simpsons.js
+++ back-end/secao-node/dia-01-runtime-assincrono/exercicio-3/my-scripts/simpsons.js
@@ -6,9 +6,26 @@ async function readAll() {
   const strings = simpsons.map(({ id, name}) => `${id} - ${name}`);
   strings.forEach((string) => console.log(string));
 }
+async function getSimpsonById(id) {
+  const fileContent = await fs
+    .readFile('./simpsons.json', 'utf-8');
+  
+  const simpsons = JSON.parse(fileContent);
+
+  const chosenSimpson = simpsons.find((simpson) => Number(simpson.id) === id);
+
+  if (!chosenSimpson) {
+    throw new Error('id não encontrado');
+  }
+
+  return chosenSimpson;
+
+}
 
 async function main() {
-  await readAll();
+  const simpson = await getSimpsonById(9);
+  console.log(simpson);
+  // await readAll();
 }
 
 main();
