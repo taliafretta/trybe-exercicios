14a35529171052f047adb12b712b9302a72eaebf
Talia Fretta <frettatalia@gmail.com>
2023-09-10 12:06:00 -0300

    alterando o arquivo contendo infos dos personagens

--- back-end/secao-node/dia-01-runtime-assincrono/exercicio-3/my-scripts/simpsons.js
+++ back-end/secao-node/dia-01-runtime-assincrono/exercicio-3/my-scripts/simpsons.js
@@ -22,10 +22,24 @@ async function getSimpsonById(id) {
 
 }
 
+async function filterSimpsons() {
+  const fileContent = await fs
+    .readFile('./simpsons.json', 'utf-8');
+
+  const simpsons = JSON.parse(fileContent);
+
+  const newArray = simpsons.filter((simpson) => simpson.id !== '10' && simpson.id !== '6');
+  await fs.writeFile('./simpsons.json', JSON.stringify(newArray));
+
+}
+
 async function main() {
-  const simpson = await getSimpsonById(9);
-  console.log(simpson);
+  // const simpson = await getSimpsonById(9);
+  // console.log(simpson);
   // await readAll();
+
+  await filterSimpsons();
+
 }
 
 main();
