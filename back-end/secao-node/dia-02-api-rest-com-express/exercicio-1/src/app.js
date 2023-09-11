07ef0e7ac852c3fd7cd219f0b6a2764be05c206d
Talia Fretta <frettatalia@gmail.com>
2023-09-11 10:00:31 -0300

    endpoint do tipo PUT com a rota /movies/:id

--- back-end/secao-node/dia-02-api-rest-com-express/exercicio-1/src/app.js
+++ back-end/secao-node/dia-02-api-rest-com-express/exercicio-1/src/app.js
@@ -33,13 +33,28 @@ app.post('/movies', async (req, res) => {
     const newMovie = {
       id: movies[movies.length - 1].id + 1,
       movie,
-      valor,
+      price,
     }
     const allMovies = JSON.stringify([...movies, newMovie]);
     await fs.writeFile(moviesPath, allMovies);
   res.status(201).json(newMovie);
   } catch (err) {
     res.status(500).send({ message: err.message});
+  }
+})
+
+app.put('/movies/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { movie, price } = req.body;
+    const movies = await readFile();
+    const index = movies.findIndex((element) => element.id === Number(id) );
+    movies[index] = { id: Number(id), movie, price };
+    const updatedMovies = JSON.stringify(movies, null, 2);
+    await fs.writeFile(moviesPath, updatedMovies);
+    res.status(200).json(movies[index]);
+  } catch (err) {
+    res.status(500).send({ message: err.message });
   }
 })
 
